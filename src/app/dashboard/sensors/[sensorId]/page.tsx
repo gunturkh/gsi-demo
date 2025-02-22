dadcc474691490c5c4dd575f14ed0004475da19c
Guntur KH <guntur.kh@gmail.com>
2025-02-22 23:15:37 +0700

    fix: type error

--- src/app/dashboard/sensors/[sensorId]/page.tsx
+++ src/app/dashboard/sensors/[sensorId]/page.tsx
@@ -1,9 +1,10 @@
+import { PageProps } from '.next/types/app/layout';
 import { delay } from '@/constants/mock-api';
 import { SensorAreaChart } from '@/features/sensors/components/sensor-area-chart';
 
 export default async function SensorPage({
   params
-}: {
+}: PageProps & {
   params: { sensorId: string };
 }) {
   const { sensorId } = await params;
